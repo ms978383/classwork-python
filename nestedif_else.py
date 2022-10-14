35205a08ed1bfe32a50ee269f453190a6c819363
ms978383 <smaheshparmar3026@gmail.com>
2022-10-14 19:38:52 +0530

    Create nestedif_else.py
    14-10-22

---
+++ nestedif_else.py
@@ -0,0 +1,39 @@
+#nested_if_else
+
+'''email="maheshkumar123@gmail.com"
+password=123
+if email=="maheshkumar123@gmail.com":
+    print("welcome ..."+email)
+    if password==123:
+        print("open email")
+    else:
+        print("please! try next time..")
+else:
+    print("look at your's email address")'''
+
+
+
+'''email="maheshkumar123@gmail.com"
+password=123
+if email=="maheshkumar123@gmail.com":
+    print("welcome ..."+email)
+    if password==1234:
+        print("open email")
+    else:
+        print("please! try next time..")
+else:
+    print("look at your's email address")'''
+
+
+email="maheshkumar123@gmail.com"
+password=123
+if email=="maheshkumar1234@gmail.com":
+    print("welcome ..."+email)
+    if password==123:
+        print("open email")
+    else:
+        print("please! try next time..")
+else:
+    print("look at your's email address")
+
+
