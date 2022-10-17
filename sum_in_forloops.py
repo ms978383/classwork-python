7212524b35f67cd74e41f440e02d05b96eb1c933
ms978383 <smaheshparmar3026@gmail.com>
2022-10-17 17:24:50 +0530

    Create sum_in_forloops.py

---
+++ sum_in_forloops.py
@@ -0,0 +1,21 @@
+n=int(input("enter no:"))
+
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+print("sum:",sum)
+"""
+n=int(input("enter a value:"))
+
+evensum=0
+oddsum=0
+for i in range(1,n+1):
+    if i%2==0:
+        evensum=evensum+i
+    else:
+        oddsum=oddsum+i
+print("evensum:",evensum)
+print("oddsum:",oddsum)
+"""
+
+            
