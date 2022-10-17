30a76642595fdd62e4dd052656f4c60dc59e65b8
ms978383 <smaheshparmar3026@gmail.com>
2022-10-17 17:23:47 +0530

    Create patterndemo.py

---
+++ patterndemo.py
@@ -0,0 +1,10 @@
+"""for i in range(1,15): for right triangle
+    print("*"*i)
+
+for i in range(1,15):    for left triangle
+    print(" "*(14-i),"*"*i)
+"""
+for i in range(1,10):
+    print(" "*(9-i)," *"*i)
+for i in range (9,0,-1):
+    print(" "*(9-i)," *"*i)
