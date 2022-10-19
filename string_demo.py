36e0cfb1a8e61399b516f3114ccee9735f6ab367
ms978383 <smaheshparmar3026@gmail.com>
2022-10-19 17:00:55 +0530

    Create string_demo.py

---
+++ string_demo.py
@@ -0,0 +1,20 @@
+s="tops career center"
+for i in range(2):
+    print(s)
+print(s.center(60,"*"))
+print(s.capitalize())
+print(s.find("en"))
+print(s.endswith("er"))
+print(s.endswith("es"))
+print(s.startswith("top"))
+print(s.swapcase())
+print("good".replace("g","d"))
+print("career center".title())
+print(s.istitle())
+print("tops".isalnum())
+print(s.center(60,"*"))
+print("career".isnumeric())
+print(s.casefold())
+print(s.upper())
+
+    
