2efccb18265ef9419cccf1ae434999f1a7267460
ms978383 <smaheshparmar3026@gmail.com>
2022-11-06 11:53:26 +0530

    Create ListSlicingDemo.py
    21-10-22

---
+++ ListSlicingDemo.py
@@ -0,0 +1,30 @@
+l=[1,2,3,1.1,2.2,3.3,"tops",True,0,1,2,True,False,"python"]
+
+print(l)
+print()
+print()
+
+print("for positive list slicing".center(70,"*"))
+
+print()
+print()
+
+print(l[1:9])
+print(l[:7])
+print(l[13:])
+print(l[2:5:2])
+print(l[::4])
+
+print()
+
+print("for negative list slicing".center(70,"*"))
+
+print()
+print()
+
+print(l[-4:-12])
+print(l[:-6])
+print(l[-4:])
+print(l[-3:-12:-2])
+print(l[::-1])
+
