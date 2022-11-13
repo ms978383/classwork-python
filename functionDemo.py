9b01e03a3a77c00bb4c46085c8fd0f19515e9487
ms978383 <smaheshparmar3026@gmail.com>
2022-11-13 22:33:57 +0530

    Create functionDemo.py

---
+++ functionDemo.py
@@ -0,0 +1,24 @@
+def printLine():
+    print("*"*80)
+
+printLine()
+print("welcome user define function".center(70,"*"))
+printLine()
+
+def add(a,b):
+    print("addition:",(a+b))
+a=int(input("enter value of a:"))
+b=int(input("enter value of b:"))
+
+add(a,b)
+printLine()
+
+def sub(n1,n2):
+    return (n1-n2)
+n1=int(input("enter value of n1:"))
+n2=int(input("enter value of n2:"))
+
+print("subtraction:",(a-b))
+printLine()
+
+    
